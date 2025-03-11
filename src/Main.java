10672aa09d86aa7ea5e69da4ded1e1bcf72eb407
AVTokarev <avtokarevvv@gmail.com>
2025-03-11 10:43:54 +0300

    Рефакторинг кода: вынес создание файлов и папок в отдельные методы

--- src/Main.java
+++ src/Main.java
@@ -1,100 +1,79 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.List;
 
 public class Main {
+    private static final StringBuilder log = new StringBuilder();
+
     public static void main(String[] args) {
-        //Task1
-        //1
-        File games = new File("Games");
-        StringBuilder states = new StringBuilder();
+        String basePath = "C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games";
 
-        File src = new File(games, "scr");
-        File res = new File(games, "res");
-        File saveGames = new File(games, "saveGames");
-        File temp = new File(games, "temp");
+        List<String> directories = List.of(
+                basePath + "\\src", basePath + "\\res", basePath + "\\saveGames", basePath + "\\temp",
+                basePath + "\\src\\main", basePath + "\\src\\test",
+                basePath + "\\res\\drawables", basePath + "\\res\\vectors", basePath + "\\res\\icons"
+        );
 
-        List<File> gamesFiles = List.of(new File[]{src, res, saveGames, temp});
-        for (File file : gamesFiles) {
-            if (file.mkdir()) {
-                states.append("Каталог ").append(file.getName()).append(" успешно создан.\n");
-            }
-        }
-        //2
-        File main = new File(src, "main");
-        File test = new File(src, "test");
+        List<String> files = List.of(
+                basePath + "\\src\\main\\Main.java", basePath + "\\src\\main\\Utils.java",
+                basePath + "\\temp\\temp.txt"
+        );
 
-        List<File> srcFiles = List.of(new File[]{main, test});
-        for (File file : srcFiles) {
-            if (file.mkdir()) {
-                states.append("Каталог ").append(file.getName()).append(" успешно создан.\n");
-            }
-        }
-        //3
-        File mainFile1 = new File(main, "Main.java");
-        File mainFile2 = new File(main, "Utils.java");
+        createDirectories(directories);
+        createFiles(files);
+        writeLogToFile(basePath + "\\temp\\temp.txt");
 
-        List<File> mainFiles = List.of(new File[]{mainFile1, mainFile2});
-        for (File file : mainFiles) {
-            try {
-                if (file.createNewFile()) {
-                    states.append("Файл ").append(file.getName()).append(" успешно создан.\n");
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        //4
-        File drawables = new File(res, "drawables");
-        File vectors = new File(res, "vectors");
-        File icons = new File(res, "icons");
-
-        List<File> resFiles = List.of(new File[]{drawables, vectors, icons});
-        for (File file : resFiles) {
-            if (file.mkdir()) {
-                states.append("Каталог ").append(file.getName()).append(" успешно создан.\n");
-            }
-        }
-        //5
-        File tempFile1 = new File(temp, "temp.txt");
-        try {
-            if (tempFile1.createNewFile()) {
-                states.append("Файл ").append(tempFile1.getName()).append(" успешно создан.\n");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        try (FileWriter writer = new FileWriter(tempFile1)) {
-            writer.write(states.toString());
-            writer.flush();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        //Task2
+        // Task 2
         GameProgress game1 = new GameProgress(60, 1, 3, 32.1);
         GameProgress game2 = new GameProgress(100, 2, 5, 12.0);
         GameProgress game3 = new GameProgress(40, 1, 1, 13.1);
 
-        GameProgress.saveGame("C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames\\save1.dat", game1);
-        GameProgress.saveGame("C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames\\save2.dat", game2);
-        GameProgress.saveGame("C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames\\save3.dat", game3);
+        List<String> saveFiles = List.of(
+                basePath + "\\saveGames\\save1.dat",
+                basePath + "\\saveGames\\save2.dat",
+                basePath + "\\saveGames\\save3.dat"
+        );
 
-        List<String> files = List.of(new String[]{
-                "C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames\\save1.dat",
-                "C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames\\save2.dat",
-                "C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames\\save3.dat"
-        });
-        GameProgress.zipFiles("C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames\\zip.zip", files);
+        GameProgress.saveGame(saveFiles.get(0), game1);
+        GameProgress.saveGame(saveFiles.get(1), game2);
+        GameProgress.saveGame(saveFiles.get(2), game3);
 
-        //Task3
-        GameProgress.openZip("C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames\\zip.zip",
-                "C:\\Users\\Анатолий\\IdeaProjects\\TheBestJavaDeveloper\\Games\\saveGames");
-        for (String file: files) {
+        GameProgress.zipFiles(basePath + "\\saveGames\\zip.zip", saveFiles);
+
+        // Task 3
+        GameProgress.openZip(basePath + "\\saveGames\\zip.zip", basePath + "\\saveGames");
+        for (String file : saveFiles) {
             System.out.println(GameProgress.openProgress(file));
+        }
+    }
+
+    private static void createDirectories(List<String> directories) {
+        for (String path : directories) {
+            File dir = new File(path);
+            if (dir.mkdir()) {
+                log.append("Каталог ").append(dir.getName()).append(" успешно создан.\n");
+            }
+        }
+    }
+
+    private static void createFiles(List<String> files) {
+        for (String path : files) {
+            File file = new File(path);
+            try {
+                if (file.createNewFile()) {
+                    log.append("Файл ").append(file.getName()).append(" успешно создан.\n");
+                }
+            } catch (IOException e) {
+                log.append("Ошибка при создании файла ").append(file.getName()).append(": ").append(e.getMessage()).append("\n");
+            }
+        }
+    }
+
+    private static void writeLogToFile(String logFilePath) {
+        try (FileWriter writer = new FileWriter(logFilePath)) {
+            writer.write(log.toString());
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи лога: " + e.getMessage());
         }
     }
 }
